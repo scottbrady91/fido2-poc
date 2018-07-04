3da143248c1a93a7d0825235022c01e4b2a44065
Scott Brady <scottbrady91@outlook.com>
2018-07-04 08:14:28 +0100

    Using statement for signature decoding

--- ScottBrady91.Fido2.Poc/Controllers/AccountController.cs
+++ ScottBrady91.Fido2.Poc/Controllers/AccountController.cs
@@ -289,27 +289,29 @@ namespace ScottBrady91.Fido2.Poc.Controllers
             // Thanks to: https://crypto.stackexchange.com/questions/1795/how-can-i-convert-a-der-ecdsa-signature-to-asn-1
             var s = Base64Url.Decode(signature);
 
-            var ms = new MemoryStream(s);
-            var header = ms.ReadByte();
-            var b1 = ms.ReadByte();
+            using (var ms = new MemoryStream(s))
+            {
+                var header = ms.ReadByte();
+                var b1 = ms.ReadByte();
 
-            var markerR = ms.ReadByte();
-            var b2 = ms.ReadByte();
-            var vr = new byte[b2];
-            ms.Read(vr, 0, vr.Length);
-            vr = RemoveAnyNegativeFlag(vr);
+                var markerR = ms.ReadByte();
+                var b2 = ms.ReadByte();
+                var vr = new byte[b2];
+                ms.Read(vr, 0, vr.Length);
+                vr = RemoveAnyNegativeFlag(vr);
 
-            var markerS = ms.ReadByte();
-            var b3 = ms.ReadByte();
-            var vs = new byte[b3];
-            ms.Read(vs, 0, vs.Length);
-            vs = RemoveAnyNegativeFlag(vs);
+                var markerS = ms.ReadByte();
+                var b3 = ms.ReadByte();
+                var vs = new byte[b3];
+                ms.Read(vs, 0, vs.Length);
+                vs = RemoveAnyNegativeFlag(vs);
 
-            var parsedSignature = new byte[vr.Length + vs.Length];
-            vr.CopyTo(parsedSignature, 0);
-            vs.CopyTo(parsedSignature, vr.Length);
+                var parsedSignature = new byte[vr.Length + vs.Length];
+                vr.CopyTo(parsedSignature, 0);
+                vs.CopyTo(parsedSignature, vr.Length);
 
-            return parsedSignature;
+                return parsedSignature;
+            }
         }
 
         private byte[] RemoveAnyNegativeFlag(byte[] input)
