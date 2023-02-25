e451f4160b86b12a250d2bfde5e0672c2f000c71
Scott Brady <scott@scottbrady91.com>
2023-02-25 16:52:10 +0000

    Added ClientDataParser

---
+++ src/ScottBrady.Fido2/Parsers/ClientDataParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json.Nodes;
+
+namespace ScottBrady.Fido2.Parsers;
+
+// TODO: documentation
+public class ClientDataParser
+{
+    public ClientData Parse(ReadOnlySpan<byte> clientDataJson)
+    {
+        // TODO: argument checking
+        var parsedObject = JsonNode.Parse(clientDataJson)?.AsObject() ?? throw new ArgumentException();
+        
+        var type = parsedObject["type"]?.GetValue<string>();
+        var challenge = parsedObject["challenge"]?.GetValue<string>();
+        var origin = parsedObject["origin"]?.GetValue<string>();
+        var crossOrigin = parsedObject["crossOrigin"]?.GetValue<bool>();
+        
+        // TODO: take copy clientDataJSON?
+        return new ClientData
+        {
+            Type = type,
+            Challenge = challenge,
+            Origin = origin,
+            CrossOrigin = crossOrigin ?? false
+        };
+    }
+}
+
+/// <summary>
+/// Parsed clientDataJSON from registration or authentication.
+/// Excludes optional TokenBinding member.
+/// </summary>
+public class ClientData
+{
+    public string Type { get; set; }
+    public string Challenge { get; set; }
+    public string Origin { get; set; }
+    public bool CrossOrigin { get; set; }
+}
---
+++ test/ScottBrady.Fido2.Tests/HappyPathTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using ScottBrady.Fido2.Parsers;
+using Xunit;
+
+namespace ScottBrady.Fido2.Tests;
+
+// Basic happy path tests using data from Windows Hello
+public class HappyPathTests
+{
+    private readonly byte[] testClientDataJson = Convert.FromBase64String("eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiVjJwUlduTE94Yi03UV9WYzVCNDk1USIsIm9yaWdpbiI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDAiLCJjcm9zc09yaWdpbiI6ZmFsc2V9");
+    private readonly byte[] testAttestationObject = Convert.FromBase64String("o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YVikSZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NFAAAAAAAAAAAAAAAAAAAAAAAAAAAAICVrJnYJupd8EWxwKQVuGJuh6RrCDEYDMvlND9ww67qRpQECAyYgASFYIID3OZq4HO2dCKLVsbYoCdwSAhgcoxnMBPV5Si0ryBaMIlggoXBwitSsaA4PKUEIfMAHcDQLFgkgqdqNZMcJ3gXKETY=");
+    
+    [Fact]
+    public void ClientDataParser_Parse()
+    {
+        var sut = new ClientDataParser();
+        var clientData = sut.Parse(testClientDataJson);
+
+        clientData.Type.Should().Be("webauthn.create");
+        clientData.Origin.Should().Be("https://localhost:5000");
+        clientData.Challenge.Should().Be("V2pRWnLOxb-7Q_Vc5B495Q");
+        clientData.CrossOrigin.Should().Be(false);
+    }
+    
+}
